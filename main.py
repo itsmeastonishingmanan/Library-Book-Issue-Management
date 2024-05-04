c6f4154a02eb43930eb5b32d465cb95686f36159
itsmeastonishingmanan <121456565+itsmeastonishingmanan@users.noreply.github.com>
2024-05-04 16:50:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+#Enter your secret key
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+Bootstrap5(app)
+
+##CREATE DB
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+##CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+## After adding the new_movie the code needs to be commented out/deleted.
+## So you are not trying to add the same movie twice. The db will reject non-unique movie titles.
+new_movie = Movie(
+    title="Phone Booth",
+    year=2002,
+    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    rating=7.3,
+    ranking=10,
+    review="My favourite character was the caller.",
+    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+)
+with app.app_context():
+    db.session.add(new_movie)
+    db.session.commit()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
